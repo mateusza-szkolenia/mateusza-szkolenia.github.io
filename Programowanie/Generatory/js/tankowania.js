73d29d3cc1f75059c2631b25a7cfaa9fb253b2d3
Mateusz Adamowski <mateusz@adamowski.pl>
2020-10-27 00:12:16 +0100

    lepsza randomizacja parametrów

--- Programowanie/Generatory/js/tankowania.js
+++ Programowanie/Generatory/js/tankowania.js
@@ -2,15 +2,15 @@
 
 let GeneratorTankowania = {
     "parametry" : {
-		"liczba_tankowan" : 20,
-		"spalanie_min" : 6.0,
-		"spalanie_max" : 12.0,
-		"dni_min" : 2,
-		"dni_max" : 20,
-		"cena_min" : 4.2,
-		"cena_max" : 5.1,
-		"litr_min" : 35.0,
-		"litr_max" : 55.0
+		"liczba_tankowan" : Random.int( 7, 25 ),
+		"spalanie_min" : Random.float( 4.0, 7.0 ),
+		"spalanie_max" : Random.float( 10.0, 15.0 ),
+		"dni_min" : Random.int( 1, 5 ),
+		"dni_max" : Random.int( 15, 30 ),
+		"cena_min" : Random.float( 4.0, 4.2 ),
+		"cena_max" : Random.float( 5.0, 5.3 ),
+		"litr_min" : Random.float( 10, 30 ),
+		"litr_max" : Random.float( 50, 70 )
     },
 
 	generuj : function(){
@@ -19,8 +19,11 @@ let GeneratorTankowania = {
 
 		let tankowania = [];
 
+        const sp_min = Random.float( this.parametry.spalanie_min, ( this.parametry.spalanie_max + this.parametry.spalanie_min ) / 2 );
+        const sp_max = Random.float( ( this.parametry.spalanie_max + this.parametry.spalanie_min ) / 2, this.parametry.spalanie_max );
+
 		for ( let i = 0; i < this.parametry.liczba_tankowan; i++ ){
-			let spalanie = Random.float( this.parametry.spalanie_min, this.parametry.spalanie_max );
+			let spalanie = Random.float( sp_min, sp_max );
 			let paliwo = Random.float( this.parametry.litr_min, this.parametry.litr_max );
 			let cena = Random.float( this.parametry.cena_min, this.parametry.cena_max );
 			droga += paliwo/spalanie * 100.0;
