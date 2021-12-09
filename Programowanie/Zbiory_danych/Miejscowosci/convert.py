4e615161cd22d81c866f80c19b80b67929e45d1b
Mateusz Adamowski <mateusz@adamowski.pl>
2021-12-09 23:10:11 +0100

    powiaty

--- Programowanie/Zbiory_danych/Miejscowosci/convert.py
+++ Programowanie/Zbiory_danych/Miejscowosci/convert.py
@@ -13,41 +13,46 @@ import locale
 import os
 import zipfile
 
-locale.setlocale(locale.LC_COLLATE, "pl_PL.UTF-8")
-
 YEAR = f'{datetime.date.today().year}'
 OUTPUT = f'data/{YEAR}-{output_fn}'
 
+def znajdz_pole(el, nazwa):
+    return [*el.findall(f'.//{{*}}{nazwa}')][0].text
+
 def process_single(el):
-    try:
-        rodzaj = [*el.findall('.//{*}rodzajObiektu')][0].text
-        assert rodzaj == 'miasto'
-        nazwa = [*el.findall('.//{*}nazwaGlowna')][0].text
-        gps = [*el.findall('.//{*}wspolrzedneGeograficzne')][0].text
-        woj = [*el.findall('.//{*}wojewodztwo')][0].text
-        lat, lon = [float(f"{float(x):.4f}") for x in gps.split(' ')]
-        return {
-            'n': nazwa,
-            'gps': [lat, lon],
-            'w': woj
-        }
-    except:
+    rodzaj = znajdz_pole(el, 'rodzajObiektu')
+    if rodzaj != 'miasto':
         return None
+    nazwa = znajdz_pole(el, 'nazwaGlowna')
+    gps = znajdz_pole(el, 'wspolrzedneGeograficzne')
+    woj = znajdz_pole(el, 'wojewodztwo')
+    powi = znajdz_pole(el, 'powiat')
+    lat, lon = [float(f"{float(x):.4f}") for x in gps.split(' ')]
+    miasto = {
+        'n': nazwa,
+        'gps': [lat, lon],
+        'w': woj,
+        'p': powi
+    }
+    return miasto
+
 try:
     os.mkdir('data')
 except FileExistsError:
     pass
 
+locale.setlocale(locale.LC_COLLATE, "pl_PL.UTF-8")
+
 try:
     miasta = json.load(open(f'{OUTPUT}.json'))
-except:
+except FileNotFoundError:
     zf = zipfile.ZipFile(zip_fn)
     input_f = zf.open(data_fn)
     tree = ET.parse(input_f)
     fC = tree.getroot()
 
     miasta = []
-    for n, fM in enumerate(fC):
+    for fM in fC:
         m = process_single(fM)
         if m:
             miasta.append(m)
@@ -55,6 +60,10 @@ except:
 
 
 miasta.sort(key=lambda m:locale.strxfrm(m['n']))
+
+wojewodztwa = [*{m['w'] for m in miasta}]
+wojewodztwa.sort(key=lambda w:locale.strxfrm(w))
+wojewodztwa = {n: (i + 1) for (i, n) in enumerate(wojewodztwa)}
 
 with open(f'{OUTPUT}.json', 'w') as f:
     json.dump(miasta, f, indent=4, ensure_ascii=False)
@@ -74,25 +83,23 @@ with open(f'{OUTPUT}.csv', 'w') as f:
 with open(f'{OUTPUT}.yml', 'w') as f:
     yaml.dump(miasta, stream=f, allow_unicode=True)
 
-    wojewodztwa = [*{m['w'] for m in miasta}]
-    wojewodztwa.sort(key=lambda w:locale.strxfrm(w))
-    wojewodztwa = {n: i for (i, n) in enumerate(wojewodztwa)}
-
 SQL_TEMPLATE = """
+-- 
+-- 
+
 Begin;
 
-LOL TEST: {{test}}
-
 Create Table `wojewodztwa`(
-    `id`    Integer Primary Key,
-    `nazwa` Text Unique Not Null
+    `id`        Integer Primary Key,
+    `nazwa`     Text Unique Not Null
 );
 
 Create Table `miasta`(
     `nazwa`     Text Not Null,
     `lat`       Float Not Null,
     `lon`       Float Not Null,
-    `woj_id`    Integer Not Null
+    `woj_id`    Integer Not Null,
+    `powiat`    Text Not Null
 );
 
 {% for (nazwa, id) in wojewodztwa.items() %}
@@ -100,7 +107,7 @@ Insert Into `wojewodztwa` Values ({{id}}, '{{nazwa}}');
 {%- endfor %}
 
 {% for m in miasta %}
-Insert Into `miasta` Values ('{{m.n}}', {{m.gps[0]}}, {{m.gps[1]}}, {{wojewodztwa[m.woj]}});
+Insert Into `miasta` Values ('{{m.n}}', {{m.gps[0]}}, {{m.gps[1]}}, {{wojewodztwa[m.w]}}, '{{m.p}}');
 {%- endfor %}
 
 Commit;
